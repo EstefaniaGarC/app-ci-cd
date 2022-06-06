c4e303dd3057f5c289ed4f9c8b4d44261857e4c9
estefania.garcia <estefania.garcia@thoughtworks.com>
2022-06-06 09:36:23 +0200

    feat: create CD a main with automatical release and run prettier

--- release.config.js
+++ release.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-    branches: "main",
-    repositoryUrl: "https://github.com/EstefaniaGarC/app-ci-cd",
-    plugins: [
-        '@semantic-release/commit-analyzer',
-        '@semantic-release/release-notes-generator',
-        '@semantic-release/github']
-}
+  branches: "main",
+  repositoryUrl: "https://github.com/EstefaniaGarC/app-ci-cd",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+  ],
+};
 
 /* According to this configuration our commits need to follow the conventional-commit nomenclature: https://www.conventionalcommits.org/en/v1.0.0/
     <type>[optional scope]: <description>
